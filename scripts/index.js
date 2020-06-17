7f6408d6c2e039673ce492deb93a12163762b2bc
Arcywojak <maciej778@op.pl>
2020-06-17 09:16:12 +0200

    add animation of tracking img

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 
+///////////////FORM HANDLING //////////////////
 const newsletterForm = document.querySelector(".newsletter-form");
 
 newsletterForm.addEventListener("submit", (e) => {
@@ -12,3 +13,85 @@ newsletterForm.addEventListener("submit", (e) => {
 
     newsletterButton.classList.add("submitted");    
 })
+/////////////////////////////////////////////////
+
+///////////// TRACKING IMG ANIMATION /////////////
+
+const trackingImg = document.querySelector(".tracking-img");
+
+const parentFirstOfTrackingImg = document.querySelector(".subsection-second.with-img.first");
+
+const parentSecondOfTrackingImg = document.querySelector(".subsection-second.with-img.second");
+
+
+
+
+const img = parentFirstOfTrackingImg.childNodes[1];
+
+
+document.addEventListener("scroll", () => {
+
+    //console.log(parentSecondOfTrackingImg.getBoundingClientRect().top)
+    /* ATTACH IMG TO SECOND SUBSECTION  */
+    if(parentSecondOfTrackingImg.getBoundingClientRect().top - 100 <= 20){
+
+        trackingImg.classList.remove("fixed-img");
+
+        if(parentFirstOfTrackingImg?.childNodes[1]?.classList?.contains("tracking-img") ||
+           parentFirstOfTrackingImg?.childNodes[2]?.classList?.contains("tracking-img")
+        ){
+
+            console.log("FIRST HAVE")
+
+            const img = parentFirstOfTrackingImg.childNodes[1];
+
+            parentFirstOfTrackingImg.removeChild(img);
+            parentSecondOfTrackingImg.appendChild(img);
+
+        }    
+    }
+    //////////////////////////////////////
+
+    /* ATTACH IMG BETWEEN SECTIONS  */
+    else if(parentFirstOfTrackingImg.getBoundingClientRect().top - 100 <= 20){
+
+     /*   if(!parentFirstOfTrackingImg?.childNodes[1]?.classList?.contains("tracking-img")){
+
+            const img = parentSecondOfTrackingImg.childNodes[1];
+
+            parentSecondOfTrackingImg.removeChild(img);
+            parentFirstOfTrackingImg.appendChild(img);
+
+        }*/
+
+        trackingImg.classList.add("fixed-img");
+        
+
+    }
+    //////////////////////////////////////
+
+    /* ATTACH IMG TO FIRST SUBSECTION */
+    else if(parentFirstOfTrackingImg.getBoundingClientRect().top - 100 > 20) {
+
+        trackingImg.classList.remove("fixed-img");
+
+        if(!parentFirstOfTrackingImg?.childNodes[1]?.classList?.contains("tracking-img") &&
+           !parentFirstOfTrackingImg?.childNodes[2]?.classList?.contains("tracking-img")
+        ){
+
+            const img = parentSecondOfTrackingImg.childNodes[1];
+
+            console.log("SECOND HAVE")
+
+          
+
+            parentSecondOfTrackingImg.removeChild(img);
+            parentFirstOfTrackingImg.appendChild(img);
+
+        }
+       
+    }
+    /////////////////////////////////////////
+})
+
+//////////////////////////////////////////////////
